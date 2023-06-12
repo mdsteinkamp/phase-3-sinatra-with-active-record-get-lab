dd02553f9bb8af13e87133b6a1ddc95ac51a055d
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-06-12 09:10:05 -0700

    commit tree a1e47aad509fe929a8317244cf3da5609cbdbeb9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do 
+    bakery = Bakery.all
+    bakery.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.all.order(:price).reverse
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    priciest_pastry = BakedGood.all.order(:price).last
+    priciest_pastry.to_json
+  end
 
 end
